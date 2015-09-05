f1fdc6e9050fd782a1459837d46a6b78cb07de3b
Nate Pauzenga <npauzenga@gmail.com>
2015-09-05 14:51:35 +0000

    added new resource button, page (does not currently add records)

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,4 +2,23 @@ class ResourcesController < ApplicationController
   def index
     @resources = Resource.all
   end
+  
+  def new
+    @resource = Resource.new
+  end
+  
+  def create
+    @resource = Resource.new(resource_params)
+    if @resource.save
+      #TODO
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def resource_params
+      params.require(:title).permit(:description, :link)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :resources
-  root 'resources#index'
+  root      'resources#index'
+  get       'new'=> 'resources#new'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
